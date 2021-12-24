f7cc502aecc9c2ebb1983d3adbeb4ec4a591b17d
Carlos <capz2003@gmail.com>
2021-12-24 18:01:17 -0500

    sets changes to new repo

---
+++ mir-express3/controllers/tareas.js
@@ -0,0 +1,39 @@
+import Tareas from "../models/data.js";
+//import {Task} from '../models/writereaddata.js'
+
+const addTarea = (req, res) => {
+  res.status(200).render("../views/index");
+};
+
+
+const newTarea = (req, res) => {
+  const { titulo, tarea } = req.body;
+  Tareas.push({ id: Tareas.length + 1, titulo, tarea });
+  console.log(Tareas);
+  res.status(201).redirect("/nuevatareas");
+};
+
+const viewTareas = (req, res) => {
+  res.status(200).render("../views/tareas", {
+    Tareas: Tareas,
+  });
+};
+
+const getTarea = (req, res) => {
+  const { tareaId } = req.params;
+  const unicaTarea = Tareas.find((tarea) => tarea.id === Number(tareaId));
+  if (!unicaTarea) {
+    return res.status(404).send("Task Does Not Exist");
+  }
+  res.status(200).render("../views/tarea", {
+    tarea: unicaTarea,
+  });
+};
+
+export  {
+  addTarea,
+  newTarea,
+  viewTareas,
+  getTarea,
+};
+
